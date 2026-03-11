1a1d25f7652193a791cf744300e0ea289131dea8
swawedWebs <swawedwebsite@gmail.com>
2026-03-10 17:59:41 -0700

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,15 +1,8 @@
 import express, { type Express } from "express";
-import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "client/dist");
-
-  if (!fs.existsSync(distPath)) {
-    console.warn("dist folder not found, serving client folder instead");
-    app.use(express.static(path.resolve(process.cwd(), "client")));
-    return;
-  }
+  const distPath = path.resolve(process.cwd(), "client");
 
   app.use(express.static(distPath));
 
