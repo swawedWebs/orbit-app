edd6c9f80e650df513a466d0b0b8377f770ef127
swawedWebs <swawedwebsite@gmail.com>
2026-03-10 21:04:11 -0700

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,19 +1,15 @@
 import express, { type Express } from "express";
-import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(process.cwd(), "client");
 
-  if (!fs.existsSync(distPath)) {
-    console.log("dist folder not found, serving client folder instead");
-  }
+  console.log("Serving static files from:", distPath);
 
-  // Serve static files
   app.use(express.static(distPath));
 
-  // Catch-all route for SPA routing
-  app.get("/*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  // Catch-all route
+  app.use((req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
