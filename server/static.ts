b0f3a3732921e1e7f9f3ee9bd7b133ca53176222
swawedWebs <swawedwebsite@gmail.com>
2026-03-10 17:28:50 -0700

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "client");
+  const distPath = path.resolve(process.cwd(), "client/dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -13,8 +13,8 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  // fallback to index.html
+  app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
